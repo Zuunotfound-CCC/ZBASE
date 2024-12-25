3f98c69a765fccb946e075af40f3fca48b591b58
Zuunotfound-CCC <lisiana000@gmail.com>
2024-12-26 04:49:03 +0900

    Add files via upload

---
+++ config/global.js
@@ -0,0 +1,27 @@
+global.name = "亥 Zbase";
+global.number = "";
+global.dev = process.env.NODE_ENV === "development";
+global.locale = "id-ID";
+global.timezone = "Asia/Jakarta";
+global.commandPath = "./cmds";
+global.creds = "./zzy/creds";
+global.store = "./zzy/store.json";
+global.prefix = ".";
+global.splitArgs = "|";
+
+global.owner = {
+  name: "Zstore",
+  number: ["6283890875133"],
+  socialMedia: [
+    {
+      name: "YouTube",
+      url: "https://youtube.com/@ZUU.00",
+    },
+  ],
+};
+
+global.mess = {
+  dev: "ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ ᴍᴀꜱɪʜ ᴅᴀʟᴀᴍ ᴛᴀʜᴀᴘ ᴘᴇɴɢᴇᴍʙᴀɴɢᴀɴ",
+  onlyOwner: "ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ ʜᴀɴʏᴀ ᴅᴀᴘᴀᴛ ᴅɪᴊᴀʟᴀɴᴋᴀɴ ᴏʟᴇʜ ᴏᴡɴᴇʀ",
+  onlyGroup: "ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ ʜᴀɴʏᴀ ʙɪꜱᴀ ᴅɪᴊᴀʟᴀɴᴋᴀɴ ᴅᴀʟᴀᴍ ɢʀᴏᴜᴘ",
+};
